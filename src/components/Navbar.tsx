92562215f3f986f7fc49831f603b51fd8d065944
Azriel <azrihell.l@gmail.com>
2024-01-10 17:16:53 -0600

    added logo, fixed tailwind bugs

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,20 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-// import './styles.css/navbar.css'
 
 const Navbar = () => {
   return (
-    <div className='items-center justify-between hidden w-full md:flex md:w-auto md:order-1'>
-      <div className='Home'>
-        <Link to="/">Home</Link>
-      </div>
-      <div className='About'>
-        <Link to="/About">About</Link>
-      </div>
-      <div className='Contact'>
-        <Link to="/Contact">Contact</Link>
+    <div className='h-full flex flex-col border-r shadow-sm'>
+      <div className='p-4 pb-2 flex justify-between items-center'>
+        <img src='' className='w-32' alt='logo'></img>
+        <div className='Home'>
+          <Link to="/">Home</Link>
+        </div>
+        <div className='About'>
+          <Link to="/About">About</Link>
+        </div>
+        <div className='Contact'>
+          <Link to="/Contact">Contact</Link>
+        </div>
       </div>
     </div>
   )
