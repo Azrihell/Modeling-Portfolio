0a994de4da2313c35b8b4ce3fbccf8189d35d72f
Azriel <azrihell.l@gmail.com>
2024-01-10 18:14:41 -0600

    navbar work

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,17 +3,21 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div className='h-full flex flex-col border-r shadow-sm'>
-      <div className='p-4 pb-2 flex justify-between items-center'>
-        <img src='' className='w-32' alt='logo'></img>
-        <div className='Home'>
-          <Link to="/">Home</Link>
-        </div>
-        <div className='About'>
-          <Link to="/About">About</Link>
-        </div>
-        <div className='Contact'>
-          <Link to="/Contact">Contact</Link>
+    <div className='h-screen'>
+      <div className='h-full inline-flex flex-col border-r'>
+        <div className='p-4 pb-2 flex items-center'>
+          <img src='https://www.azcraftscode.com/assets/azLogo-18289bf9.png' className='w-32' alt='logo'></img>
+          <div className='relative py-2 px-3 cursor-pointer'>
+            <div className='p-4 m-5 bg-gray-700 hover:bg-gray-600 rounded'>
+              <Link to="/">Home</Link>
+            </div>
+            <div className='p-4 m-5 bg-gray-700 hover:bg-gray-600 rounded '>
+              <Link to="/About">About</Link>
+            </div>
+            <div className='p-4 m-5 bg-gray-700 hover:bg-gray-600 rounded'>
+              <Link to="/Contact">Contact</Link>
+            </div>
+          </div>
         </div>
       </div>
     </div>
