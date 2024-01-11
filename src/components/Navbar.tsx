212d98f1fa96a5c252d1690860a3ce00bc7000c7
Azriel <azrihell.l@gmail.com>
2024-01-10 19:02:45 -0600

    navbar changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import azlogo from '../assets/azlogo.png'
 
 const Navbar = () => {
   return (
     <div className='h-screen'>
-      <div className='h-full inline-flex flex-col border-r'>
-        <div className='p-4 pb-2 flex items-center'>
-          <img src='https://www.azcraftscode.com/assets/azLogo-18289bf9.png' className='w-32' alt='logo'></img>
-          <div className='relative py-2 px-3 cursor-pointer'>
+      <div className='flex'>
+        <div className='p-4 pb-2'>
+          <img src={azlogo} className='w-96' alt='logo'></img>
+          <div className='fixed lg: py-2 px-3 cursor-pointer'>
             <div className='p-4 m-5 bg-gray-700 hover:bg-gray-600 rounded'>
               <Link to="/">Home</Link>
             </div>
