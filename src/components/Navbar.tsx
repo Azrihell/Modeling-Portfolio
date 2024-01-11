092b495dbe467280a967bfc032784903ff4c0753
Azriel <azrihell.l@gmail.com>
2024-01-11 16:19:49 -0600

    constants folder added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,20 +1,26 @@
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import azlogo from '../assets/azlogo.png'
 
+
 const Navbar = () => {
+
+  const [active, setActive] = useState('')
+  const [toggle, setToggle] = useState(false)
+
   return (
     <div className='h-screen'>
       <div className='flex'>
         <div className='p-4 pb-2'>
           <img src={azlogo} className='w-96' alt='logo'></img>
-          <div className='fixed lg: py-2 px-3 cursor-pointer'>
-            <div className='p-4 m-5 bg-gray-700 hover:bg-gray-600 rounded'>
+          <div className='fixed py-2 px-3 cursor-pointer'>
+            <div className='p-4 m-5 bg-gray-700 hover:bg-gray-600 active:bg-pink-200 rounded'>
               <Link to="/">Home</Link>
             </div>
-            <div className='p-4 m-5 bg-gray-700 hover:bg-gray-600 rounded '>
+            <div className='p-4 m-5 bg-gray-700 hover:bg-gray-600 active:bg-pink-200 rounded '>
               <Link to="/About">About</Link>
             </div>
-            <div className='p-4 m-5 bg-gray-700 hover:bg-gray-600 rounded'>
+            <div className='p-4 m-5 bg-gray-700 hover:bg-gray-600 active:bg-pink-200 rounded'>
               <Link to="/Contact">Contact</Link>
             </div>
           </div>
